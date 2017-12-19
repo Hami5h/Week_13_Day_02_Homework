1096c77b4cbd7593572061f58fe3dfbec5ece3c5
Hamish <hamishfpso@gmail.com>
2017-12-19 17:04:24 +0000

    Create and fetch whisky working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const server = express();
 
 server.use(parser.json());
 server.use(express.static('client/build'));
@@ -12,5 +13,36 @@ MongoClient.connect('mongodb://localhost:27017', function(err, client) {
     return;
   }
   const db = client.db("whisky");
-  console.log('Connect to databse');
-})
+  console.log("Connect to databse");
+
+  server.post('/api/whiskys', function(req, res) {
+    db.collection('whiskys').insert(req.body, function(err, result) {
+      if(err) {
+        console.log(err);
+        res.status(500);
+        res.send();
+        return;
+      }
+      console.log('Saved to database');
+      res.status(200);
+      res.json(result.ops[0]);
+    });
+  });
+
+  server.get('/api/whiskys', function(req, res) {
+    db.collection('whiskys').find().toArray(function(err, result) {
+      if(err) {
+        console.log(err);
+        res.status(500);
+        res.send();
+        return;
+      }
+      res.json(result);
+    });
+  });
+
+
+  server.listen(3000, function() {
+    console.log("Listeneing on port 3000");
+  })
+});
